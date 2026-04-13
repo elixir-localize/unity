acb4177d1a6a2d885d926ce4f3806fd41a649448
Kip Cole <kipcole9@gmail.com>
2026-04-13 11:45:33 +0100

    Add more memory debug output

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,17 @@
 Localize.put_default_locale(:en)
+
+# Diagnostic: print literal allocator state before tests run
+if System.get_env("CI") do
+  [{:instance, 0, sections}] = :erlang.system_info({:allocator, :literal_alloc})
+  {_, options} = List.keyfind(sections, :options, 0)
+  {_, mbcs} = List.keyfind(sections, :mbcs, 0)
+  IO.puts("\n=== literal_alloc (in test BEAM) ===")
+  IO.puts("  options: #{inspect(Keyword.take(options, [:mmbcs, :smbcs, :lmbcs, :sbct]))}")
+  IO.puts("  mbcs: #{inspect(mbcs)}")
+
+  mmap_info = :erlang.system_info({:allocator, :erts_mmap})
+  IO.puts("  erts_mmap: #{inspect(mmap_info, limit: :infinity, width: 200)}")
+  IO.puts("===\n")
+end
+
 ExUnit.start(exclude: [:integration])
